a568c77ef181b2b13a7445f26dbd91d8355b90d5
Mauro Rimondetti <micorreo@gmail.com>
2024-07-17 20:50:23 -0300

    first commit

---
+++ appcoder/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def inicio(request):
+    return render(request, "appcoder/inicio.html")
+
+def cursos(request):
+    return render(request, "appcoder/cursos.html")
+
+def estudiantes(request):
+    return HttpResponse("Vista de estudiantes")
+
+def profesores(request):
+    return HttpResponse("Vista de los profesoress")
+
+def entregables(request):
+    return HttpResponse("Vista de los entregables")
